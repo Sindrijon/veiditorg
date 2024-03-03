e4089ae88eaa5c737e33138a347ef771fcecb8e2
PannaSigrun <annasg19@ru.is>
2024-03-03 22:46:59 +0000

    SignupFragment base work, not ready

--- app/src/main/java/com/example/veiditorg/SignupFragment.kt
+++ app/src/main/java/com/example/veiditorg/SignupFragment.kt
@@ -5,11 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 
-// TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+private const val ARG_USERNAME_INPUT = "param1"
+private const val ARG_FULLNAME_INPUT = "param2"
+private const val ARG_PASSWORD_INPUT = "param3"
+private const val ARG_EMAIL_INPUT = "param4"
+private const val ARG_PHONE_INPUT = "param5"
 
 /**
  * A simple [Fragment] subclass.
@@ -17,16 +21,22 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class SignupFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    lateinit var usernameInput : EditText
+    lateinit var fullnameInput : EditText
+    lateinit var passwordInput : EditText
+    lateinit var emailInput : EditText
+    lateinit var phoneInput : EditText
+    lateinit var signupBtn : Button
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        usernameInput = view.findViewById(R.id.signupUsername)
+        fullnameInput = view.findViewById(R.id.signupFullname)
+        passwordInput = view.findViewById(R.id.signupPassword)
+        emailInput = view.findViewById(R.id.signupEmail)
+        phoneInput = view.findViewById(R.id.signupPhone)
+        signupBtn = view.findViewById(R.id.signupButton)
     }
 
     override fun onCreateView(
@@ -44,15 +54,20 @@ class SignupFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
+         * @param param3 Parameter 3.
+         * @param param4 Parameter 4.
+         * @param param5 Parameter 5.
          * @return A new instance of fragment SignupFragment.
          */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance(param1: String, param2: String, param3: String, param4: String, param5: String) =
             SignupFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+                    putString(ARG_USERNAME_INPUT, param1)
+                    putString(ARG_FULLNAME_INPUT, param2)
+                    putString(ARG_PASSWORD_INPUT, param3)
+                    putString(ARG_EMAIL_INPUT, param4)
+                    putString(ARG_PHONE_INPUT, param5)
                 }
             }
     }
