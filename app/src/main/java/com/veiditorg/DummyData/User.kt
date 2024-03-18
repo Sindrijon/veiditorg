696454f3713ce697ea7eed7d44cafc63ae7ea753
PannaSigrun <annasg19@ru.is>
2024-03-18 02:38:52 +0000

    User data klasa bætt við

---
+++ app/src/main/java/com/veiditorg/DummyData/User.kt
@@ -0,0 +1,9 @@
+package com.veiditorg.DummyData
+// Simple data class that holds user information
+data class User(
+    val userID: String,
+    val fullName: String,
+    val password: String,
+    val email: String,
+    val phone: String
+)
