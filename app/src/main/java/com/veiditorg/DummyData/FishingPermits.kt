83e646a46e624d45ef498779d3425aff069acd4f
PannaSigrun <annasg19@ru.is>
2024-03-24 18:38:38 +0000

    Lagaði FishingPermits

--- app/src/main/java/com/veiditorg/DummyData/FishingPermits.kt
+++ app/src/main/java/com/veiditorg/DummyData/FishingPermits.kt
@@ -4,11 +4,12 @@ import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 
 class FishingPermits(
-    private val permits: MutableList<Permit>,
-    private var idCounter: Int = 1
+    private val permits: MutableList<Permit>
 ) {
     // Contains all fishing permits in a mutable list
     class PermitViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    var idCounter: Int = 1
 
     fun addPermit(permit: Permit) {
         permit.setID(idCounter)
@@ -16,17 +17,7 @@ class FishingPermits(
         permits.add(permit)
     }
 
-    fun addPermits(listOfPermits: MutableList<Permit>) {
-        for (permit in listOfPermits) {
-            this.addPermit(permit)
-        }
-    }
-
     fun deletePermit(permit: Permit) {
         permits.remove(permit)
-    }
-
-    fun getItemCount(): Int {
-        return permits.size
     }
 }
