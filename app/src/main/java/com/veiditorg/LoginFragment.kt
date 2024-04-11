8fe003773d4459be88753204d4603c757fbfabae
sindrijon <sindrijons@gmail.com>
2024-04-11 01:23:40 +0000

    bætt við signup og login tengingu við db + virkni

--- app/src/main/java/com/veiditorg/LoginFragment.kt
+++ app/src/main/java/com/veiditorg/LoginFragment.kt
@@ -8,18 +8,23 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.example.veiditorg.R
 import com.example.veiditorg.databinding.FragmentLoginBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
 class LoginFragment : Fragment() {
 
-    lateinit var usernameInput : EditText
+    private lateinit var mAuth: FirebaseAuth
+
+    lateinit var useremailInput : EditText
+
     lateinit var passwordInput : EditText
     lateinit var loginBtn : Button
     private var _binding: FragmentLoginBinding? = null
@@ -28,10 +33,6 @@ class LoginFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
-    private fun isValidCredentials(username: String, password: String): Boolean {
-        //Checks if user exists in list of registered users
-        return MainActivity.allUsers.validUser(username, password)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -45,12 +46,13 @@ class LoginFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        mAuth = FirebaseAuth.getInstance()
 
         //fjarlægir navigationbar frá LoginFragment
         val bottomNavigationView = activity?.findViewById<BottomNavigationView>(R.id.bottomNavigationView)
         bottomNavigationView?.visibility = View.GONE
 
-        usernameInput = view.findViewById(R.id.editTextUsername)
+        useremailInput = view.findViewById(R.id.editTextUsername)
         passwordInput = view.findViewById(R.id.editTextPassword)
         loginBtn = view.findViewById(R.id.loginButton)
         val currentActivity = activity
@@ -66,15 +68,27 @@ class LoginFragment : Fragment() {
 
         loginBtn.setOnClickListener {
 
-            val username = usernameInput.text.toString()
+            val email = useremailInput.text.toString()
             val password = passwordInput.text.toString()
 
-            if (isValidCredentials(username, password) && currentActivity !=null) {
-                    val transaction = currentActivity.supportFragmentManager.beginTransaction()
-                    transaction.replace(R.id.frame_layout, MarketplaceFragment())
-                    transaction.commit()
+            if (email.isNotEmpty() && password.isNotEmpty())
+
+                loginUser(email, password)
+            else
+                Toast.makeText(context, "Fylla út email og pw", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun loginUser(email: String, pass: String) {
+        mAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener {task->
+            if (task.isSuccessful) {
+
+                val transaction = parentFragmentManager.beginTransaction()
+                transaction.replace(R.id.frame_layout, MarketplaceFragment())
+                transaction.addToBackStack(null)
+                transaction.commit()
             } else {
-                Snackbar.make(view, "Invalid username or password", Snackbar.LENGTH_SHORT).show()
+                Toast.makeText(requireContext(), task.exception.toString(), Toast.LENGTH_SHORT).show()
             }
         }
     }
@@ -82,6 +96,10 @@ class LoginFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun init(view: View) {
+        mAuth = FirebaseAuth.getInstance()
     }
 }
 
