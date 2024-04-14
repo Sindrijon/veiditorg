db36865fa6e10a5d86776447c00de0a0d2f567e7
Gudrunmaria <gudrunmaria@gmail.com>
2024-04-14 11:59:06 +0000

    birti inventory og bætti við TradeFragment

--- app/src/main/java/com/veiditorg/TradeFragment.kt
+++ app/src/main/java/com/veiditorg/TradeFragment.kt
@@ -1,21 +1,12 @@
 package com.veiditorg
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.veiditorg.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.veiditorg.adapter.MyAdapter
-import com.veiditorg.modul.PermitViewModel
-import androidx.lifecycle.Observer
-import com.google.firebase.auth.FirebaseAuth
-
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,16 +15,11 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [HomepageFragment.newInstance] factory method to
+ * Use the [TradeFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class HomepageFragment : Fragment() {
-
-    private lateinit var viewModel: PermitViewModel
-    private lateinit var permitRecyclerView: RecyclerView
-    lateinit var adapter: MyAdapter
-    private lateinit var auth: FirebaseAuth
-
+class TradeFragment : Fragment() {
+    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
@@ -50,7 +36,7 @@ class HomepageFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_homepage, container, false)
+        return inflater.inflate(R.layout.fragment_trade_fragment, container, false)
     }
 
     companion object {
@@ -60,12 +46,12 @@ class HomepageFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment HomepageFragment.
+         * @return A new instance of fragment TradeFragmetn.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            HomepageFragment().apply {
+            TradeFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
@@ -78,28 +64,5 @@ class HomepageFragment : Fragment() {
 
         val bottomNavigationView = activity?.findViewById<BottomNavigationView>(R.id.bottomNavigationView)
         bottomNavigationView?.visibility = View.VISIBLE
-
-
-        auth = FirebaseAuth.getInstance()
-        val currentUser = auth.currentUser
-        val ownerId = currentUser?.uid ?: ""
-
-        permitRecyclerView = view.findViewById(R.id.homepageRecyclerview)
-        permitRecyclerView.layoutManager = LinearLayoutManager(context)
-        permitRecyclerView.setHasFixedSize(true)
-        adapter = MyAdapter()
-        permitRecyclerView.adapter = adapter
-
-        viewModel = ViewModelProvider(this).get(PermitViewModel::class.java)
-
-        viewModel.allPermits.observe(viewLifecycleOwner, Observer {allPermits ->
-
-            val currentUserPermits = allPermits.filter { permit ->
-                permit.ownerId==ownerId
-            }
-            Log.d("Marketplace", "Permits list updated: ${currentUserPermits.size} items")
-
-            adapter.updatePermitList(currentUserPermits)
-        })
     }
 }
